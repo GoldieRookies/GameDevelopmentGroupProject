1473f7e1c2d0eaa453e5a184e67906ef05b89bd8
GoldieRookies <59270979+GoldieRookies@users.noreply.github.com>
2023-03-28 15:26:14 -0400

    aaa

--- GameDevProjectPostonItch/Assets/EnemyFollowScript.cs
+++ GameDevProjectPostonItch/Assets/EnemyFollowScript.cs
@@ -20,9 +20,9 @@ public class EnemyFollowScript : MonoBehaviour
     {
 
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        Vector3 angleTarget = Vector3.RotateTowards(transform.position, player.transform.position, speedOfRotation * Time.deltaTime, 0.0f);
+        /*Vector3 angleTarget = Vector3.RotateTowards(transform.position, player.transform.position, speedOfRotation * Time.deltaTime, 0.0f);
 
-        transform.rotation = Quaternion.LookRotation(angleTarget);
+        transform.rotation = Quaternion.LookRotation(angleTarget);*/
     }
 
 }
